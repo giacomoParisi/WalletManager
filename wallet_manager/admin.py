c04622336c05d550eaf36462c0f1adc8191a76e8
Giacomo Parisi <giacomoparisi95@gmail.com>
2021-12-18 15:55:33 +0100

    Updated transaction element name

--- wallet_manager/admin.py
+++ wallet_manager/admin.py
@@ -3,7 +3,7 @@ from .models import Transaction
 
 # Register your models here.
 class TransactionModelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'type', 'descritpion', 'date', 'created_at', 'last_modified')
+    list_display = ('id', 'type', 'descritpion', 'value', 'date', 'created_at', 'last_modified')
     readonly_fields = ('id',)
 
 admin.site.register(Transaction, TransactionModelAdmin)
--- wallet_manager/models.py
+++ wallet_manager/models.py
@@ -11,6 +11,10 @@ class Transaction(models.Model):
         default=TransactionType.MISCELLANEOUS,
     )
     descritpion = models.CharField(max_length=50)
+    value = models.FloatField()
     date = models.DateField(auto_now=False)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id) + '-' + self.descritpion
