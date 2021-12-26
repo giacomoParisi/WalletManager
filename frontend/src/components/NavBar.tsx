d16ef777f4a3eadcbb3523830876b0034afff6ca
Giacomo Parisi <giacomoparisi95@gmail.com>
2021-12-26 14:09:11 +0100

    Added nav bar compoment

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+import logo from "./assets/images/logo.png"
+
+const NavBar = () => {
+  <nav className="navbar navbar-light bg-light">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">
+        <img
+          src={logo}
+          alt=""
+          width="30"
+          height="24"
+          className="d-inline-block align-text-top"
+        />
+        Bootstrap
+      </a>
+    </div>
+  </nav>;
+};
+
+export default NavBar;
