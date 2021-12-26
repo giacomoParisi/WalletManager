d29353d599c4745f853ba47d84316f221b8702fe
Giacomo Parisi <giacomoparisi95@gmail.com>
2021-12-26 14:42:12 +0100

    Added React Router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,12 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Test from "./routes/Test";
+
+const rootElement = document.getElementById("root");
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="test" element={<Test/>} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
