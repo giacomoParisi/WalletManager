2710bad41931fa6e5d7e00a1f8e8d375fba27052
Giacomo Parisi <giacomoparisi95@gmail.com>
2021-12-30 18:21:25 +0100

    Fixed lint errors

--- frontend/src/app/slicer.ts
+++ frontend/src/app/slicer.ts
@@ -1,24 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { Themes } from "../themes/themes";
-import { RootState } from "./store";
+import { createSlice } from '@reduxjs/toolkit'
+import { Themes } from '../themes/themes'
+import { RootState } from './store'
 
 export const slicer = createSlice({
-  name: "app",
-  initialState: {
-    theme: Themes.LIGHT,
-  },
-  reducers: {
-    darkTheme: (state) => {
-      state.theme = Themes.DARK;
+    name: 'app',
+    initialState: {
+        theme: Themes.LIGHT,
     },
-    lightTheme: (state) => {
-      state.theme = Themes.LIGHT;
+    reducers: {
+        darkTheme: (state) => {
+            state.theme = Themes.DARK
+        },
+        lightTheme: (state) => {
+            state.theme = Themes.LIGHT
+        },
     },
-  },
-});
+})
 
-export const { darkTheme, lightTheme } = slicer.actions;
+export const { darkTheme, lightTheme } = slicer.actions
 
-export const selectorTheme = (state: RootState) => state.app.theme
+export const selectorTheme: (state: RootState) => Themes = (state: RootState) =>
+    state.app.theme
 
 export default slicer.reducer
--- frontend/src/components/switch/switch.tsx
+++ frontend/src/components/switch/switch.tsx
@@ -1,32 +1,28 @@
-import './switch.css';
+import './switch.css'
 
- 
 type SwitchProps = {
-    isOn: boolean,
+    isOn: boolean
     icon: string
-    handleToggle: () => void,
+    handleToggle: () => void
 }
 
-const Switch = ({isOn, icon, handleToggle} : SwitchProps) => {
+function Switch({ isOn, icon, handleToggle }: SwitchProps) {
     return (
-      <>
-        <input
-          checked={isOn}
-          onChange={handleToggle}
-          className="react-switch-checkbox"
-          id={`react-switch-new`}
-          type="checkbox"
-        />
-        <label
-          className="react-switch-label"
-          htmlFor={`react-switch-new`}
-        >
-          <div className="react-switch-button" >
-              <img className='switch-icon' src={icon}  />
-          </div>
-        </label>
-      </>
-    );
-  };
-  
-  export default Switch;
+        <>
+            <input
+                checked={isOn}
+                onChange={handleToggle}
+                className="react-switch-checkbox"
+                id="react-switch-new"
+                type="checkbox"
+            />
+            <label className="react-switch-label" htmlFor="react-switch-new">
+                <div className="react-switch-button">
+                    <img className="switch-icon" src={icon} alt="" />
+                </div>
+            </label>
+        </>
+    )
+}
+
+export default Switch
