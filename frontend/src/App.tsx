7b5b27cbb6f4d85937de658ea46fc802a9714c4c
Giacomo Parisi <giacomoparisi95@gmail.com>
2021-12-30 17:57:23 +0100

    Added prettier and eslint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,27 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./routes/Home";
-import Test from "./routes/Test";
-import NavBar from "./components/navbar/navbar";
-import { ThemeProvider } from "styled-components";
-import { useSelector } from 'react-redux';
-import getTheme from "./themes/themeProvider";
-import { selectorTheme } from "./app/slicer";
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { useSelector } from 'react-redux'
+import Home from './routes/Home'
+import Test from './routes/Test'
+import NavBar from './components/navbar/navbar'
+import getTheme from './themes/themeProvider'
+import { selectorTheme } from './app/slicer'
 
-const App = () => { 
+function App() {
+    const theme = useSelector(selectorTheme)
 
-  const theme = useSelector(selectorTheme)
+    return (
+        <ThemeProvider theme={getTheme(theme)}>
+            <Router>
+                <NavBar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/test" element={<Test />} />
+                </Routes>
+            </Router>
+        </ThemeProvider>
+    )
+}
 
-  return (
-      <ThemeProvider theme={getTheme(theme)}>
-        <Router>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/test" element={<Test />} />
-          </Routes>
-        </Router>
-      </ThemeProvider>
-  );
-};
-
-export default App;
+export default App
